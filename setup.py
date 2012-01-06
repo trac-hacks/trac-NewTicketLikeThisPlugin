905be8774d3132fd90f799e69e70b72138cd2fb9
Ethan Jucovy <ejucovy@gmail.com>
2012-01-06 10:05:52 -0500

    add license and long_description metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import find_packages, setup
 
-version='0.0'
+version='0.1'
+
+try:
+    long_description = open("README.txt").read()
+except:
+    long_description = ''
 
 setup(name='NewTicketLikeThisPlugin',
       version=version,
-      description="",
+      description="Pluggable framework for 'cloning' Trac tickets according to custom business logic",
+      long_description=long_description,
       author='Ethan Jucovy',
       author_email='ejucovy@gmail.com',
-      url='',
+      url='http://trac-hacks.org/wiki/NewTicketLikeThisPlugin',
       keywords='trac plugin',
-      license="",
+      license="BSD",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
       include_package_data=True,
       package_data={ 'newticketlikethisplugin': ['templates/*', 'htdocs/*'] },
