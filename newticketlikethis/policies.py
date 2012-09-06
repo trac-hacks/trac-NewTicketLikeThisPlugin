4ef201e2a51033bdf15340318d014538c18752bb
Ethan Jucovy <ejucovy@gmail.com>
2012-09-06 14:12:11 +0000

    implement a new policy that lets the user specify genshi templates to derive new ticket fields from the old ticket's fields

--- newticketlikethis/policies.py
+++ newticketlikethis/policies.py
@@ -1,3 +1,4 @@
+from genshi.template.text import NewTextTemplate
 from trac.config import ListOption
 from trac.core import Component, implements
 from trac.util.translation import _
@@ -55,3 +56,30 @@ class ExcludedFieldsTicketCloner(Component):
                 fields[name] = ticket[name]
         return fields
 
+class DerivedFieldsTicketCloner(Component):
+    """
+    An ITicketCloner implementation that lets users specify a derivation
+    of new fields from the values of the original ticket's fields.
+
+    Derivations are specified in configuration like so::
+
+    [newticketlikethis]
+    derived_fields = $ticket.id->original_ticket_id, $ticket.component $ticket.reporter->keywords
+    """
+
+    implements(ITicketCloner)
+
+    derived_fields = ListOption('newticketlikethis', 'derived_fields')
+
+    def build_clone_form(self, req, ticket, data):
+        fields = {}
+        for derivation in self.derived_fields:
+            template, new_field = derivation.split('->')
+            template = NewTextTemplate(template.encode('utf8'))
+            fields[new_field] = template.generate(ticket=ticket).render('text', encoding=None).strip()
+        for f in data.get('fields', []):
+            name = f['name']
+            if name in fields:
+                continue
+            fields[name] = ticket[name]
+        return fields
